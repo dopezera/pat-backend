f0c6570ba552a3e74f4711e03f95f758ea3e9bdc
dopezera <dopezera@gmail.com>
2021-06-30 19:47:29 -0300

    arrumando sequelize

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-const sequelize = new Sequelize('naO4DU6RTQ', 'naO4DU6RTQ', 'bgFpmONy4u', {
+const sequelize = new Sequelize('naO4DU6RTQ', 'naO4DU6RTQ', 'kAVVk1mlPj', {
     host: 'remotemysql.com',
     port: '3306',
     dialect: 'mysql',
