f7f04d82fa97e27c861339b45837fd008891c445
dopezera <dopezera@gmail.com>
2022-03-23 15:09:26 -0300

    separating routes from business logic - 1st route

--- routes.js
+++ routes.js
@@ -1,15 +1,20 @@
 import userRouter from './routers/userRouter.js';
-import matchRouter from './routers/matchRouter.js';
 import eventRouter from './routers/eventRouter.js';
-
+import express from 'express'
+import sequelize from './db.js'
 import pstatsLoader from './fakedb/pstatsLoader.js';
 
-import express from 'express'
+import getMatches from './controllers/matches.js';
 
 const router = express.Router()
 
+router.get('/api/matches', (req, res) => {
+  getMatches(req, res)
+})
+
+/*
+
 router.use('/api/users', userRouter);
-router.use('/api/matches', matchRouter);
 router.use('/api/events', eventRouter);
 
 router.use('/api/pstats', pstatsLoader);
@@ -18,6 +23,6 @@ router.get('/', (req, res) => {
   res.send('ok')
 })
 
-
+*/
 
 export default router
--- server.js
+++ server.js
@@ -9,32 +9,29 @@ import passport from 'passport'
 
 import routes from './routes.js'
 import cookieParser from 'cookie-parser'
-import jwt from 'express-jwt' 
-
+//import jwt from 'express-jwt' 
+import verifyAuth from './controllers/account.js'
 
 dotenv.config()
 const app = express()
 
 
-
-
-
-
-//app.use(cors({ origin: 'https://mixstats.herokuapp.com', credentials: true}));
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true})); //tem que ter cors setado para origem da requisição senão cookies vai flopar. tem gambiarra mas nao vale a pena (cors refletido é vulnerabilidade).
 app.use(express.json()) //allow json in the body of requests (signin backend in basir's video)
 app.use(express.urlencoded({extended: true})) //this line allows us to access information from a form. with this 2 middleware all requests that contain data will translate to req.body
 app.use(bodyParser.json())
 
 app.use(cookieParser());
-app.use(
-  jwt({
-    secret: config.SECRET_KEY,
-    getToken: req => req.headers['authorization'].split(' ')[1],
-    algorithms: ['HS256']
-  })
-);
 
+
+app.use(verifyAuth)
+
+
+//github pull requests. request para o dono do projeto fazer o pull das minhas modificações (proposta de mudança do código).
+//usar eslint e prettier
+
+//jwt decode pra pegar user e salvar em req.user (exemplo)
+//
 
 
 /** BAGUI DA STEAM */
@@ -42,16 +39,13 @@ app.use(
 app.set('view engine', 'ejs')
 app.set('views', '/views')
 
-
-
 app.use(passport.initialize())
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+
 app.use(routes)
-
-
 
 
 app.listen(config.PORT, () => {
